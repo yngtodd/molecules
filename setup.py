98d3b272ec32b497e45a91c46ce5d2eaebaba60a
yngtodd <young.todd.mk@gmail.com>
2018-10-22 15:18:38 -0400

    Updating version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ history = open('HISTORY.rst').read().replace('.. :changelog:', '')
 
 setup(
     name='molecules',
-    version='0.0.2',
+    version='0.0.3',
     description='Machine learning for molecular dynamics.',
     long_description=readme + '\n\n' + doclink + '\n\n' + history,
     author='Todd Young',
