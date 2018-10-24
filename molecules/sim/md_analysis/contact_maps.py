844515029e47a58ff481e32e5ff5cd12f1448432
hengma <38470774+hengma1001@users.noreply.github.com>
2018-10-24 11:59:33 -0400

    Fixed the cm_table shape

--- molecules/sim/md_analysis/contact_maps.py
+++ molecules/sim/md_analysis/contact_maps.py
@@ -16,6 +16,7 @@ def contact_maps_from_traj(xyz_file, traj_file, cm_table_file, contact_cutoff=8.
         raise ValueError('Please specify a path to save contact maps.')
 
     mda_traj = mda.Universe(xyz_file, traj_file)
+    traj_length = len(mda_traj.trajectory) 
     ca = mda_traj.select_atoms('name CA')
     num_ca = ca.n_atoms
     contact_size = int(num_ca * (num_ca - 1) / 2)
@@ -24,7 +25,7 @@ def contact_maps_from_traj(xyz_file, traj_file, cm_table_file, contact_cutoff=8.
         outpath = os.path.join(savepath, filename)
         outfile = tables.open_file(outpath, 'w')
         atom = tables.Float64Atom()
-        cm_table = outfile.create_earray(outfile.root, 'contact_maps', atom, shape=(contact_size, 0)) 
+        cm_table = outfile.create_earray(outfile.root, 'contact_maps', atom, shape=(traj_length, 0)) 
 
     contact_matrices = []
     for frame in mda_traj.trajectory:
