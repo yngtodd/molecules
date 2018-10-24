d0a6767ccc1d43b4df1edb5170dc570b7631ac0c
hengma <38470774+hengma1001@users.noreply.github.com>
2018-10-24 11:52:50 -0400

    Cleaning up the contact map
    cleaning up on minor bugs on save path and others

--- molecules/sim/md_analysis/contact_maps.py
+++ molecules/sim/md_analysis/contact_maps.py
@@ -15,25 +15,25 @@ def contact_maps_from_traj(xyz_file, traj_file, cm_table_file, contact_cutoff=8.
     if filename and not savepath:
         raise ValueError('Please specify a path to save contact maps.')
 
+    mda_traj = mda.Universe(xyz_file, traj_file)
+    ca = mda_traj.select_atoms('name CA')
+    num_ca = ca.n_atoms
+    contact_size = int(num_ca * (num_ca - 1) / 2)
+    
     if savepath and filename:
         outpath = os.path.join(savepath, filename)
         outfile = tables.open_file(outpath, 'w')
         atom = tables.Float64Atom()
-        cm_table = outfile.create_earray(outfile.root, 'contact_maps', atom, shape=(contact_size, 0))
-
-    mda_traj = mda.Universe(xyz_file, traj_file)
-    ca = mda_traj.select_atoms('name CA')
-    num_ca = ca.n_atoms
-    contact_size = int((num_ca - 1) / 2)
+        cm_table = outfile.create_earray(outfile.root, 'contact_maps', atom, shape=(contact_size, 0)) 
 
     contact_matrices = []
     for frame in mda_traj.trajectory:
-        thresholded_matrx = contacts.contact_matrix(distances.self_distance_array(ca.positions), radius=contact_cutoff) * 1.0
-        reshaped_matrix = thresholded_matrix.reshape(contact_size, 1)
-        contact_matrices.append(reshaped_matrix)
+        cm_matrix = contacts.contact_matrix(distances.self_distance_array(ca.positions), radius=contact_cutoff) * 1.0 
+        contact_matrices.append(cm_matrix)
 
-        if savepath and filename:
-            cm_table.append(reshaped_matrix)
-            outfile.close()
+    if savepath and filename:
+        cm_table.append(reshaped_matrix)
+        outfile.close() 
+        return outpath
 
     return contact_matrices
