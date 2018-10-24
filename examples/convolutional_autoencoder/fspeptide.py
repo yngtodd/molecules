0088fdd3743ab40111317029b957ba77d54fa781
yngtodd <young.todd.mk@gmail.com>
2018-10-23 23:06:04 -0400

    Fixing up examples

--- examples/convolutional_autoencoder/fspeptide.py
+++ examples/convolutional_autoencoder/fspeptide.py
@@ -1,4 +1,5 @@
 import argparse
+import numpy as np
 from keras.optimizers import RMSprop
 
 from molecules.ml.unsupervised import EncoderConvolution2D
@@ -18,19 +19,25 @@ def main():
 
     x_train, _ = train_data.load_data()
     x_val, _ = val_data.load_data()
-    input_shape = (21,21,1)
+
+    # Keras complains if height and width dimensions are odd.
+    x_train = np.pad(x_train, [(0,0),(0,1), (0,1), (0,0)], mode='constant')
+    x_val = np.pad(x_val, [(0,0),(0,1), (0,1), (0,0)], mode='constant')
+
+    print(f'validation shape: {x_val.shape}')
+    input_shape = (22,22,1)
 
     optimizer = RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
 
     encoder = EncoderConvolution2D(input_shape=input_shape)
 
-    #encoder._get_final_conv_params()
-    #num_conv_params = encoder.total_conv_params
-    #encode_conv_shape = encoder.final_conv_shape
+    encoder._get_final_conv_params()
+    num_conv_params = encoder.total_conv_params
+    encode_conv_shape = encoder.final_conv_shape
 
     decoder = DecoderConvolution2D(output_shape=input_shape,
-                                   enc_conv_params=7744,
-                                   enc_conv_shape=(11,11,64))
+                                   enc_conv_params=num_conv_params,
+                                   enc_conv_shape=encode_conv_shape)
 
     cvae = VAE(input_shape=input_shape,
                latent_dim=3,
