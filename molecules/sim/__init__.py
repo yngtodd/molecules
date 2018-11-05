864916d22264dfa06a6e1362dd51b316b6d06f43
yngtodd <young.todd.mk@gmail.com>
2018-11-05 16:47:53 -0500

    Updating imports

---
+++ molecules/sim/__init__.py
@@ -0,0 +1,4 @@
+from .analysis.contact_maps import contact_maps_from_traj
+
+from .simulation.openmm_simulation import openmm_simulate_charmm_nvt
+from .simulation.openmm_simulation import openmm_simulate_amber_nvt 
