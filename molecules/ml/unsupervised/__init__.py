c6a365bc92ffc1d3dc672c45239fd537de7f5e49
yngtodd <young.todd.mk@gmail.com>
2018-12-01 17:16:54 -0500

    Add hparams classes to __init__

--- molecules/ml/unsupervised/__init__.py
+++ molecules/ml/unsupervised/__init__.py
@@ -2,3 +2,6 @@ from .vae import VAE
 
 from .conv_vae.encoder import EncoderConvolution2D
 from .conv_vae.decoder import DecoderConvolution2D
+
+from .conv_vae.encoder import HyperparamsEncoder
+from .conv_vae.decoder import HyperparamsDecoder
